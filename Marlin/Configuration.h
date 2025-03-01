e4876464a7e9053320b56e6b0cea212151c7cbc7
Denis Hessberger <d.hessberger@bretgeld-engineering.de>
2025-03-01 19:48:17 +0100

    More more more...

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1264,7 +1264,7 @@
   #endif
 #endif
 
-#define DEFAULT_EJERK    5.0  // May be used by Linear Advance
+#define DEFAULT_EJERK    5  // May be used by Linear Advance
 
 /**
  * Junction Deviation Factor
@@ -1287,7 +1287,7 @@
  *
  * See https://github.com/synthetos/TinyG/wiki/Jerk-Controlled-Motion-Explained
  */
-#define S_CURVE_ACCELERATION
+//#define S_CURVE_ACCELERATION
 
 //===========================================================================
 //============================= Z Probe Options =============================
